f445ad8657b845eae27f0b29ff6ce65d5b7213e8
silenjs <silenjs@gmail.com>
2018-08-24 15:57:18 +0800

    - change useref to useref-sfbest

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var path = require('path'),
     PluginError = require('plugin-error'),
     es = require('event-stream'),
     through = require('through2'),
-    useref = require('useref'),
+    useref = require('useref-sfbest'),
     getGlobs = require('./lib/getGlobs'),
     addFilesFromExtStreams = require('./lib/addFilesFromExtStreams'),
     addHtmlToStream = require('./lib/addHtmlToStream'),
@@ -22,9 +22,11 @@ function handleAdditionalStreams(additionalStreams) {
 
     return _additionalStreams.map(function (stream) {
         // filters stream to select needed files
-        return stream.pipe(es.through(function (file) {
-            additionalFiles.push(file);
-        }));
+        return stream.pipe(
+            es.through(function (file) {
+                additionalFiles.push(file);
+            })
+        );
     });
 }
 
@@ -51,18 +53,17 @@ function addAssetsToStream(paths, files) {
     unprocessedCounter.increment();
 
     // Get relative file paths and join with search paths to send to vinyl-fs
-    globs = filepaths
-        .filter(isRelativeUrl)
-        .map(function (filepath) {
-            paths.filepath = filepath;
+    globs = filepaths.filter(isRelativeUrl).map(function (filepath) {
+        paths.filepath = filepath;
 
-            return getGlobs(paths, files);
-        });
+        return getGlobs(paths, files);
+    });
 
-    src = vfs.src(globs, {
-        base: basePath,
-        nosort: true
-    })
+    src = vfs
+        .src(globs, {
+            base: basePath,
+            nosort: true
+        })
         .on('error', function (err) {
             self.emit('error', new Error(err));
         });
@@ -85,19 +86,20 @@ function addAssetsToStream(paths, files) {
 
     // Add assets to the stream
     // If noconcat option is false, concat the files first.
-    src
-        .pipe(gulpif(!options.noconcat, concat(name, gulpConcatOptions)))
-        .pipe(through.obj(function (newFile, encoding, callback) {
-            // specify an output path relative to the cwd
-            if (options.base) {
-                newFile.path = path.join(options.base, name);
-                newFile.base = options.base;
-            }
+    src.pipe(gulpif(!options.noconcat, concat(name, gulpConcatOptions)))
+        .pipe(
+            through.obj(function (newFile, encoding, callback) {
+                // specify an output path relative to the cwd
+                if (options.base) {
+                    newFile.path = path.join(options.base, name);
+                    newFile.base = options.base;
+                }
 
-            // add file to the asset stream
-            self.push(newFile);
-            callback();
-        }))
+                // add file to the asset stream
+                self.push(newFile);
+                callback();
+            })
+        )
         .on('finish', function () {
             var unprocessed = unprocessedCounter.decrement();
 
@@ -121,14 +123,18 @@ function processAssets(file, basePath, data) {
         }
 
         for (name in files) {
-            addAssetsToStream.call(self, {
-                name: name,
-                basePath: basePath,
-                searchPath: pluginOptions.searchPath,
-                cwd: file.cwd,
-                transformPath: pluginOptions.transformPath,
-                type: type
-            }, files);
+            addAssetsToStream.call(
+                self,
+                {
+                    name: name,
+                    basePath: basePath,
+                    searchPath: pluginOptions.searchPath,
+                    cwd: file.cwd,
+                    transformPath: pluginOptions.transformPath,
+                    type: type
+                },
+                files
+            );
         }
     });
 }
@@ -152,50 +158,60 @@ module.exports = function (options) {
         waitForAssets = through.obj();
     }
 
-    return through.obj(function (file, enc, cb) {
-        var self = this;
+    return through.obj(
+        function (file, enc, cb) {
+            var self = this;
 
-        waitForAssets.pipe(es.wait(function () {
-            var output,
+            waitForAssets.pipe(
+                es.wait(function () {
+                    var output,
 
-                // Cache the file base path relative to the cwd
-                // Use later when it could be dropped
-                _basePath = path.dirname(file.path);
+                        // Cache the file base path relative to the cwd
+                        // Use later when it could be dropped
+                        _basePath = path.dirname(file.path);
 
-            if (file.isNull()) {
-                return cb(null, file);
+                    if (file.isNull()) {
+                        return cb(null, file);
+                    }
+
+                    if (file.isStream()) {
+                        return cb(
+                            new PluginError(
+                                'gulp-useref',
+                                'Streaming not supported'
+                            )
+                        );
+                    }
+
+                    output = useref(file.contents.toString(), opts);
+
+                    addHtmlToStream.call(self, file, output[0]);
+
+                    if (!opts.noAssets) {
+                        processAssets.call(self, file, _basePath, output[1]);
+                    }
+
+                    return cb();
+                })
+            );
+
+            // If no external streams were included,
+            // emit 'end' on the empty stream
+            if (!additionalStreams) {
+                waitForAssets.emit('end');
+            }
+        },
+        function (cb) {
+            var unprocessed = unprocessedCounter.get(),
+                fn = function () {};
+
+            end.set(cb);
+
+            if (unprocessed === 0) {
+                fn = cb;
             }
 
-            if (file.isStream()) {
-                return cb(new PluginError('gulp-useref', 'Streaming not supported'));
-            }
-
-            output = useref(file.contents.toString(), opts);
-
-            addHtmlToStream.call(self, file, output[0]);
-
-            if (!opts.noAssets) {
-                processAssets.call(self, file, _basePath, output[1]);
-            }
-
-            return cb();
-        }));
-
-        // If no external streams were included,
-        // emit 'end' on the empty stream
-        if (!additionalStreams) {
-            waitForAssets.emit('end');
+            return fn();
         }
-    }, function (cb) {
-        var unprocessed = unprocessedCounter.get(),
-            fn = function () {};
-
-        end.set(cb);
-
-        if (unprocessed === 0) {
-            fn = cb;
-        }
-
-        return fn();
-    });
+    );
 };
